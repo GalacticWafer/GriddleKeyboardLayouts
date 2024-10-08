92e3910f0586013f1dde40d9b105362d165b3c42
GalacticWafer <galacticwafer493@gmail.com>
2024-10-07 19:21:40 -0600

    saving before i mess it up badder

--- layouts/griddle/english/GriddleEnglishAlphaNumericLayer.kt
+++ layouts/griddle/english/GriddleEnglishAlphaNumericLayer.kt
@@ -3,6 +3,7 @@ package org.galacticware.griddle.keyboarddefinition.opensource.layouts.griddle.e
 import android.content.Context
 import android.graphics.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
 import org.galacticware.griddle.domain.keyboard.KeyboardHandedness
 import org.galacticware.griddle.domain.language.LanguageTag
 import org.galacticware.griddle.keyboarddefinition.system.AbstractKeyboardLayer
@@ -24,7 +25,9 @@ import org.galacticware.griddle.keyboarddefinition.opensource.layouts.griddle.sh
 import org.galacticware.griddle.keyboarddefinition.opensource.layouts.griddle.shared.numericSpaceLeft
 import org.galacticware.griddle.keyboarddefinition.opensource.layouts.griddle.shared.numericSpaceRight
 import org.galacticware.griddle.keyboarddefinition.opensource.layouts.griddle.shared.rightAlphaLayerToggle
+import org.galacticware.griddle.keyboarddefinition.opensource.layouts.griddle.shared.space
 import org.galacticware.griddle.keyboarddefinition.system.LanguageLayer
+import kotlin.math.roundToInt
 
 /**
  * All language layers should have the  [AbstractKeyboardLayer.layerDefinition] property set to [LayerDefinition.ALPHA_LAYER].
@@ -46,6 +49,12 @@ class GriddleEnglishAlphaNumericLayer(
         pivotColumn = 3,
     ),
 ) {
+    private val defaultButtonSize: IntSize = GriddleEnglishBoard.defaultButtonSize.let{
+        IntSize(
+            width = (it.width * .75).roundToInt(),
+            height = it.height,
+        )
+    }
     override val layerDefinition = LayerDefinition.ALPHA_NUMERIC_UNIFIED
     override val layerType = LayerType.ALPHA_NUMERIC_UNIFIED
     override var builders = buttonBuilders
@@ -55,13 +64,14 @@ class GriddleEnglishAlphaNumericLayer(
             button_0_0, button_0_1, button_0_2, cursorControlButton,
             button_1_0, button_1_1, button_1_2, rightAlphaLayerToggle,
             button2_0, button_2_1, button2_2, backspace,
-            numericSpaceLeft, enter, numericSpaceRight
+           space, enter,
+                   // numericSpaceLeft, enter, numericSpaceRight
         ).plus(
             setOf(
                 button1, button2, button3,
                 button4, button5, button6,
                 button7, button8, button9,
-                button0.withPosition(colStart = 0, colSpan = 3)
+                button0.withPosition(colStart = 0, colSpan = 3),
                 // right-side buttons start at column <pivot column + 1> == 4
             ).map { it.withPosition(colStart = 4 + it.positionAndSize.colStart) }
         ).toMutableSet()
